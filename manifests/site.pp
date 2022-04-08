34b98c8460dcdfaec9412cf0505ab1f205f98f2d
Teos WorkStation <teos@otaku.gmail.com>
2022-04-08 18:53:24 +0300

    one file

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,15 +2,38 @@ node default {
 }
 
 node 'master.puppet' {
-   include role::master
+   file { '/root/README':
+      ensure => absent,
+      }
 }
 
 node 'slave1.puppet' {
-   include role::slave1
+   class { 'apache': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/web1.org/html/index.html':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/te0s/hometask_new/main/sites/01-demosite-static/index.html',
+      replace => false,
+      }
 }
 
 node 'slave2.puppet' {
-   include role::slave2
+   class { 'apache::mod::php': }
+   
+   class { 'php': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.php':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/te0s/hometask_new/main/sites/01-demosite-php/index.php',
+      replace => false,
+      }
 }
-
 
