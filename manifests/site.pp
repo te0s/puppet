81e1b229f90448b3c5b7993b99642bef36123300
teos home <teos.otaku@gmail.com>
2022-04-15 23:30:42 +0300

    reincluded nginx, added se disable

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,16 +2,38 @@ node default {
 }
 
 node 'master.puppet' {
-include nginx
+
+package { 'nginx' :
+  ensure => installed,
+                   } ->
+  service {'nginx':
+  ensure => running,
+  enable => true,
+   }
+
 nginx::resource::server { 'static':
   listen_port => 80,
   proxy => 'http://192.168.56.101:80',
   }
+
 nginx::resource::server { 'dynamic':
   listen_port => 81,
   proxy => 'http://192.168.56.102:80',
   }
 }
+
+exec { 'selinux_to_permissive':
+  command     => 'setenforce 0',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user       => 'root',
+  }
+
+exec { 'reboot_nginx':
+  command     => 'systemctl restart nginx',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user => 'root',
+  }
+
 
 node 'slave1.puppet' {
    class { 'apache': }
