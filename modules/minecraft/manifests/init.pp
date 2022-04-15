17c91f01c60c8a71509bb24c68f21692da9ab3aa
Teos <teos.otaku@gmail.com>
2022-04-15 19:02:26 +0300

    added modules in puppet

---
+++ modules/minecraft/manifests/init.pp
@@ -0,0 +1,31 @@
+class minecraft {
+  package {'java-17-openjdk':
+    ensure => installed
+    }
+
+ file {'/opt/minecraft':
+   ensure => directory
+   }
+
+file {'/opt/minecraft/eula.txt':
+  ensure => file,
+  content => 'eula=true'
+     }
+     
+
+file { '/opt/minecraft/server.jar':
+  ensure => file,
+  source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+  replace => false,
+     }
+
+
+ file {'/etc/systemd/system/minecraft.service':
+   ensure => file,
+   source => 'puppet:///modules/minecraft/minecraft.service'
+   }
+
+  service {'mineserver':
+  ensure => running,
+          }
+}
