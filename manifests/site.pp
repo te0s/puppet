d1283b95b1e603ba29433d55ec9a0362e25e62e2
Teos <teos.otaku@gmail.com>
2022-04-17 17:34:17 +0300

    add fix in one file

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,7 +59,37 @@ node 'slave2.puppet' {
       }
 }
 
+node 'minecraft.puppet' {
 
-node 'mine' {
-  include minecraft
+include selinux
+
+include wget
+
+ package {'java-17-openjdk':
+  ensure => installed,
+}
+
+ file {'/opt/minecraft':
+  ensure => directory,
+}
+
+ file {'/opt/minecraft/eula.txt':
+  content => 'eula=true',
+}
+
+ wget::fetch { "minecraft-server":
+     source      => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+     destination => '/opt/minecraft/',
+     timeout     => 0,
+     verbose     => false,
+}
+
+ file {'/etc/systemd/system/minecraft-server.service':
+     ensure => file,
+     source => 'puppet:///modules/minecraft/minecraft-server.service',
+}
+ ~> service { 'minecraft-server':
+     ensure => running,
+     enable => true,
+   }
 }
